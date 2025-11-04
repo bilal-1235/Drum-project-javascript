b5d28fee89bc631d31d6786b25cf288aae0b79d9
bilal-1235 <bilal.afsarpk55@gmail.com>
2025-11-04 11:49:57 +0500

    adding the animation in the button when it got pressed

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ for (var i = 0 ; i < numberofDrumButtons; i++ ) {
         var buttonInnerHTML = this.innerHTML;
 
         makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
         
       
     });
@@ -15,6 +16,7 @@ for (var i = 0 ; i < numberofDrumButtons; i++ ) {
 
 document.addEventListener("keypress",function (event){
     makeSound(event.key);
+    buttonAnimation(event.key);
 });
 
 function makeSound(key){
@@ -52,3 +54,15 @@ switch(key){
 
         }
 };
+
+
+function  buttonAnimation(currentKey){
+    var activeButton = document.querySelector("." + currentKey);
+
+    activeButton.classList.add("pressed");
+
+    setTimeout(function(){
+        activeButton.classList.remove("pressed");
+
+    },100);
+}
